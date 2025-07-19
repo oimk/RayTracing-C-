79ab5b872dcb505f84e00f8ecf7038491c9a4b62
Gurgle <181671980+oimk@users.noreply.github.com>
2025-07-19 18:50:23 -0400

    Command Working

--- main.cc
+++ main.cc
@@ -5,7 +5,7 @@ int main()
     int image_width = 256;
     int image_height = 256;
     
-    std::cout << "P3/n" << image_width << ' ' <<image_height << "\n255\n";
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
     
     for (int j =0; j < image_height; j++) {
         for (int i  = 0; i < image_width; i++) {
